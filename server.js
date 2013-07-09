47af26db7a75238a6c2aa033b28dd19c1c29b6c9
iwa4 <iwa4dev@gmail.com>
2013-07-09 16:27:47 +0900

    show env mode

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.put   ('/wines/:id', wine.updateWine);
 app.delete('/wines/:id', wine.deleteWine);
 
 http.createServer(app).listen(app.get('port'), function() {
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port %d in %s mode", app.settings.port, app.settings.env);
 });
