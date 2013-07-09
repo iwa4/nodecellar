8211fd242b63fdd1d6e27024974b48f9d7f080cd
iwa4 <iwa4dev@gmail.com>
2013-07-09 16:26:48 +0900

    typo fix

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var app = express();
 app.configure(function() {
   app.set('port', process.env.PORT || 3000);
   app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
-  app.use(express.bodyParser()),
-          app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.bodyParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.get   ('/wines',     wine.findAll);
