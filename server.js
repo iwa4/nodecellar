4b5cd714d7b9cd99ab5c768848c02aefa69015b6
iwa4 <iwa4dev@gmail.com>
2013-07-09 16:25:20 +0900

    format

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express = require('express'),
-        path = require('path'),
-        http = require('http'),
-        wine = require('./routes/wines');
+var express = require('express')
+  , path    = require('path')
+  , http    = require('http')
+  , wine    = require('./routes/wines');
 
 var app = express();
 
@@ -12,10 +12,10 @@ app.configure(function() {
           app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.get('/wines', wine.findAll);
-app.get('/wines/:id', wine.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
+app.get   ('/wines',     wine.findAll);
+app.get   ('/wines/:id', wine.findById);
+app.post  ('/wines',     wine.addWine);
+app.put   ('/wines/:id', wine.updateWine);
 app.delete('/wines/:id', wine.deleteWine);
 
 http.createServer(app).listen(app.get('port'), function() {
